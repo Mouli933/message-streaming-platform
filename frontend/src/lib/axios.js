6a7ce8d6fcf537d1232e6f38d1f1b4041d1b38f9
Mouli933 <moulithota372@gmail.com>
2026-04-14 16:24:19 +0530

    Update axios baseURL for development and production

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+baseURL:
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5000/api"
+    : "http://44.202.35.234:5000/api",
   withCredentials: true,
 });
